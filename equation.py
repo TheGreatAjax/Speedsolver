16611488f8138428a52730b9ce3c6aa3c88c30fb
TheGreatAjax <thegreatajax@yandex.com>
2022-07-26 21:22:31 +1000

    improve generation

--- equation.py
+++ equation.py
@@ -6,7 +6,7 @@ class Equation(pg.sprite.Sprite):
 
     def __init__(self, col, font, lvlConfig, eq_group):
         super().__init__(eq_group)
-        self.range = lvlConfig.operands_range
+        self.range = random.randint(2, lvlConfig.operands_range)
         self.operands = [str(i) for i in range(1, lvlConfig.operands_max + 1)]
         self.operations = lvlConfig.operations
 
@@ -15,7 +15,6 @@ class Equation(pg.sprite.Sprite):
 
         self.__setGraphics(col, font)
 
-    
     def __setGraphics(self, col, font):
         self.image = font.render(self.repr, 0, pg.Color('white')) # Which is text
         text_w = self.image.get_width()
@@ -30,8 +29,13 @@ class Equation(pg.sprite.Sprite):
     # Generate the equation
     def generate(self):
         eq = random.choice(self.operands) # String representation of the equation
-                                                 # operand-operation-operand-...
+                                          # operand-operation-operand-...
         for _ in range (1, self.range):
-            eq += random.choice(self.operations)
+            op = random.choice(self.operations)
+            # self.operations.remove(op)    # Disallow same operations
+            eq += op
             eq += random.choice(self.operands)
+        
+        # if self.range > 2:
+
         return eq
